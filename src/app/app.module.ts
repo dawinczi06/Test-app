81d7ec726ca4388ebc88f239bbb30d6b2b4585a4
dawinczi06 <52246105+dawinczi06@users.noreply.github.com>
2020-09-07 13:40:16 +0200

    first draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { AccountsComponent } from './accounts/accounts.component';
+import { HomeComponent } from './home/home.component';
+ 
+const routes: Routes = [
+  { path: 'accounts', component: AccountsComponent},
+  { path: 'home', component: HomeComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+] 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountsComponent,
+    HomeComponent,    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterModule.forRoot(routes)  
   ],
   providers: [],
   bootstrap: [AppComponent]
